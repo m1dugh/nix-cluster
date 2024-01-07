c6a629966d3c529157a0ce556cd4018a97a05d6f
m1dugh <romain103paris@gmail.com>
2024-01-07 22:56:43 +0100

    misc: updates to nixpkgs 23.11

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
     description = "A very basic flake";
 
-    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     outputs = {
         nixpkgs,
