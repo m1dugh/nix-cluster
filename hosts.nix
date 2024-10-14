a8caac86d39debd893f5ef59c7032a2ea4407ab8
m1dugh <romain.le-miere@epita.fr>
2024-10-14 23:11:02 +0200

    fix(network): adds forward for node ports in cluster

--- hosts.nix
+++ hosts.nix
@@ -6,8 +6,10 @@ in
   extraConfigs = {
     "cluster-master-1" = {
       midugh.gateway.extraNatConfig.prerouting = ''
-        iifname "eth0" tcp dport { 80, 443 } dnat ip to 192.168.1.146;
-        iifname "wg0" tcp dport { 80, 443 } dnat ip to 192.168.1.147;
+        iifname "eth0" tcp dport 80 dnat ip to 192.168.1.146:30080;
+        iifname "eth0" tcp dport 443 dnat ip to 192.168.1.146:30443;
+        iifname "wg0" tcp dport 80 dnat ip to 192.168.1.146:31080;
+        iifname "wg0" tcp dport 443 dnat ip to 192.168.1.146:31443;
       '';
       midugh.gateway.extraNatConfig.postrouting = ''
         oifname "eth0" snat ip to 192.168.1.145;
@@ -15,10 +17,7 @@ in
       networking.firewall.allowedTCPPorts = [ 80 443 ];
     };
     "cluster-master-2" = {
-      networking.firewall.allowedTCPPorts = [ 80 443 ];
-    };
-    "cluster-master-3" = {
-      networking.firewall.allowedTCPPorts = [ 80 443 ];
+      networking.firewall.allowedTCPPorts = [ 30080 30443 31080 31443 ];
     };
   };
   deploymentConfig = {
