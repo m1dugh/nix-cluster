3fc0fd812135e9c6130d36ea5f418565ed97629e
m1dugh <romain.le-miere@epita.fr>
2024-10-14 23:03:16 +0200

    feat(coredns): adds forwarder option

--- modules/kubernetes/coredns.nix
+++ modules/kubernetes/coredns.nix
@@ -6,11 +6,20 @@
 with lib;
 let
   top = config.midugh.k8s-cluster;
+  cfg = top.coredns;
   inherit (top.nodeConfig) worker address;
   inherit (top) apiserver;
   inherit ((pkgs.callPackage ./lib.nix { }).lib) mkCoreDnsCert;
 in
 {
+options.midugh.k8s-cluster.coredns.forwarder = mkOption {
+    type = types.str;
+    default = "1.1.1.1";
+    description = ''
+        An external forwarder for the dns server
+        '';
+    example = "8.8.8.8";
+};
   config = mkIf (top.enable && worker) {
     services.coredns = {
       enable = true;
@@ -21,7 +30,7 @@ in
                 tls ${mkCoreDnsCert "coredns.pem"} ${mkCoreDnsCert "coredns-key.pem"} ${mkCoreDnsCert "ca.pem"}
                 pods verified
             }
-            forward . 1.1.1.1:53
+            forward . ${cfg.forwarder}:53
         }
       '';
     };
@@ -30,9 +39,12 @@ in
 
     networking.firewall.allowedUDPPorts = [ 53 ];
 
-    users.groups.coredns = { };
+    users.groups.coredns = {
+        gid = 993;
+    };
     users.users.coredns = {
       group = config.users.groups.coredns.name;
+      uid = config.users.groups.coredns.gid;
       isSystemUser = true;
     };
   };
